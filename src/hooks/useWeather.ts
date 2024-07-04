9f4b782c1b6614ae0c99930910d18473d92dc0be
tatianaZC <tatianazapata98@gmail.com>
2024-07-04 17:57:47 +1000

    use safe url

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -59,7 +59,7 @@ export default function useWeather() {
     setWeather(initialState)
     setNotFound(false)
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       const {data} = await axios.get(geoUrl)
 
       if(!data[0]) {
